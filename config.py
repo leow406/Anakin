d0fffa1a07c3f2f62d9bf765b22b9bc75aed9d77
leowww <163104853+leow406@users.noreply.github.com>
2025-06-02 15:19:34 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# Bot token
+TOKEN       = "YOUR BOT TOKEN"
+
+# Command prefix
+PREFIX      = "!"
+
+# ffmpeg path (or just ffmpeg if it's aleady in the PATH)
+FFMPEG_PATH = "ffmpeg"
+
+SPOTIPY_CLIENT_ID     = "CLIENT ID"
+SPOTIPY_CLIENT_SECRET = "CLIENT SECRET"
+
+# Channel where you want the player to be sent
+PLAYER_CHANNEL_ID = PLAYER CHANNEL ID
